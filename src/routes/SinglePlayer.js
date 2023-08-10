a281c9cee5a34d5ae672fda59cc8c621b64aa18b
Julian Petrich <julian.paul.petrich@rokt.com>
2023-08-10 16:13:13 +1000

    Adjusted single player

--- src/routes/SinglePlayer.js
+++ src/routes/SinglePlayer.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './NavBar';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Image from 'react-bootstrap/Image';
 import { API } from 'aws-amplify';
 import { useParams } from 'react-router-dom';
 
@@ -14,9 +18,10 @@ function PlayerDetails() {
 
   async function fetchPlayer() {
     try {
+      //TODO: Find a better solution here
       const data = await API.get('myapi', `/greeting/${id}`);
-      console.log(data);
-      setPlayer(data); // Assuming the response directly contains the player data
+      console.log(data.players[id-1]);
+      setPlayer(data.players[id-1]); // Assuming the response directly contains the player data
     } catch (error) {
       console.error('Error fetching player:', error);
     }
@@ -27,13 +32,25 @@ function PlayerDetails() {
       <NavBar />
       {player && (
         <div>
-          <h2>{player.name}</h2>
-          <div className="table-container-wrapper">
-            <p>Position: {player.position}</p>
-            <p>Games: {player.games}</p>
-            <p>Goals: {player.goals}</p>
-            {/* Render other player details if needed */}
-          </div>
+            <Card border="secondary">
+                <Card.Body>
+                  <Card.Title className="text-center">{player.name}</Card.Title>
+                  <Card.Text>
+                    <Col className='text-center'>
+                      <Image src="/mosman_logo.png" roundedCircle />
+                    </Col>
+                    <Row>
+                    <Col>Position: {player.position}</Col>
+                    <Col>Games: {player.games}</Col>
+                    <Col>Games: {player.games}</Col>
+                  </Row>
+                  <Row>
+                    <Col>Goals: {player.goals}</Col>
+                    <Col>Clean Sheets: {player.cleansheets}</Col>
+                  </Row>
+                  </Card.Text>
+                </Card.Body>
+            </Card>
         </div>
       )}
     </div>
