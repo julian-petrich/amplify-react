bff9fa69c0985b7d288b75c8d5512e20a2f5be1a
Julian Petrich <julian_petrich@yahoo.de>
2023-12-10 08:19:25 +1100

    Loading for images

--- src/routes/Carousel.js
+++ src/routes/Carousel.js
@@ -1,34 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import Image from 'react-bootstrap/Image';
 import "./Carousel.css";
 
 function CarouselImages() {
+  const [imagesLoadedCount, setImagesLoadedCount] = useState(0);
+  const [imagesLoaded, setImagesLoaded] = useState(false);
+
+  useEffect(() => {
+    const container = document.querySelector('.news-container');
+
+    const handleImageLoad = (event) => {
+      if (event.target.tagName === 'IMG') {
+        setImagesLoadedCount((prevCount) => prevCount + 1);
+      }
+    };
+
+    container.addEventListener('load', handleImageLoad);
+
+    return () => {
+      container.removeEventListener('load', handleImageLoad);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (imagesLoadedCount === document.querySelectorAll('.news-container img').length) {
+      setImagesLoaded(true);
+    }
+  }, [imagesLoadedCount]);
+
+
   return (
+ 
     <div className='news-container'>
+         {!imagesLoaded && <p>Loading...</p>}
+    {imagesLoaded && (
+      <div>
       <h1> Mosman FC - News</h1>
-      <Carousel style={{ maxWidth: '800px', margin: '0 auto' }}>
+        
+      <Carousel>
         <Carousel.Item>
           <Image src="/GF_Outside.jpeg" text="First slide" />
-          <Carousel.Caption>
+          <Carousel.Caption className='carousel-caption-top'>
             <h3>Grand Final Win 26.08.2023</h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <Image src="/GF_Inside.jpeg" text="Second slide" />
-          <Carousel.Caption>
+          <Carousel.Caption className='carousel-caption-top'>
             <h3>2-0 against Manly A</h3>
             <p>Goals: Nick, Lucas </p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <Image src="/Paz_Nick_Trophy.png" text="Third slide"/>
-          <Carousel.Caption>
+          <Image src="/Paz_Nick_Trophy.png" text="Third slide" />
+          <Carousel.Caption className='carousel-caption-top'>
             <h3>Masterminds behind the success</h3>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
-
       <div className="news-section">
 
       <div className="news-article">
@@ -53,17 +83,18 @@ function CarouselImages() {
 
         <div className="news-article">
           <div className="image-container">
-            <Image src="/mosman_mosman_c.jpeg" text="Caption for Additional Image 3" className="news-image" />
+            <Image src="/mosman_mosman_c.jpeg" text="Caption for Additional Image 3" className="news-image"  />
             <div className="image-caption-overlay">
               <h3>5-0 Win against Mosman C</h3>
               <p>Goals: Craig (2), Nick, Thjis, Mark</p>
             </div>
           </div>
         </div>
-
-        {/* Add more news articles as needed */}
       </div>
+      </div>
+         )}
     </div>
+ 
     );
 }
 
